b0a25ea693b167a21353315c2a30ed23314b1c15
Kamila <Kamila@172.20.61.5>
2016-10-07 20:08:28 -0300

    pesquisa de aluno

--- src/telas/aluno/FrmConsAluno.java
+++ src/telas/aluno/FrmConsAluno.java
@@ -1,13 +1,16 @@
 package telas.aluno;
 
-import controller.CursosController;
-import controller.DisciplinaController;
+import controller.CursosController;  
 import controller.MatriculaController;
 import controller.TurmaController;
+import facade.MatriculasFacade;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List; 
-import model.Cursos;
-import model.Disciplinas;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.Cursos; 
 import model.Matriculas;
 import model.Turmas;
 import util.Formatacao;
@@ -46,11 +49,10 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
         btnExcluir = new javax.swing.JButton();
         lblNomeCurso2 = new javax.swing.JLabel();
         lblNomeCurso3 = new javax.swing.JLabel();
-        lblNomeCurso4 = new javax.swing.JLabel();
         btnPesquisar = new javax.swing.JButton();
         cbxTurma = new javax.swing.JComboBox<String>();
         cbxCurso = new javax.swing.JComboBox<String>();
-        cbxDisciplina = new javax.swing.JComboBox<String>();
+        btnLimpar = new javax.swing.JButton();
 
         jTextField2.setAlignmentX(0.1F);
         jTextField2.setAlignmentY(0.1F);
@@ -137,15 +139,17 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
         btnExcluir.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         btnExcluir.setPreferredSize(new java.awt.Dimension(80, 90));
         btnExcluir.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirActionPerformed(evt);
+            }
+        });
 
         lblNomeCurso2.setText("Curso:");
         lblNomeCurso2.setAlignmentY(0.1F);
 
         lblNomeCurso3.setText("Turma:");
         lblNomeCurso3.setAlignmentY(0.1F);
-
-        lblNomeCurso4.setText("Disciplina:");
-        lblNomeCurso4.setAlignmentY(0.1F);
 
         btnPesquisar.setBackground(new java.awt.Color(255, 255, 255));
         btnPesquisar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/pesquisar.png"))); // NOI18N
@@ -176,10 +180,13 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
             }
         });
 
-        cbxDisciplina.setToolTipText("");
-        cbxDisciplina.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                cbxDisciplinaItemStateChanged(evt);
+        btnLimpar.setBackground(new java.awt.Color(255, 255, 255));
+        btnLimpar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/limparpesquisa.png"))); // NOI18N
+        btnLimpar.setText("Limpar");
+        btnLimpar.setBorder(null);
+        btnLimpar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimparActionPerformed(evt);
             }
         });
 
@@ -194,20 +201,20 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
                         .addComponent(jScrollPane1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(29, 29, 29)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(lblNomeCurso)
-                            .addComponent(lblNomeCurso4)
-                            .addComponent(txtNomeAluno)
-                            .addComponent(cbxDisciplina, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(cbxTurma, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lblNomeCurso3)
+                            .addComponent(lblNomeCurso)
+                            .addComponent(txtNomeAluno, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(cbxCurso, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblNomeCurso2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblNomeCurso3)
+                            .addComponent(cbxTurma, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
                         .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(34, 34, 34)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnLimpar, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(42, 42, 42)))
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(264, 264, 264)
@@ -224,27 +231,26 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnLimpar, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(17, 17, 17)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(lblNomeCurso2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(txtNomeAluno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(cbxCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(txtNomeAluno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(lblNomeCurso)
                                 .addGap(26, 26, 26)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblNomeCurso4)
-                            .addComponent(lblNomeCurso3))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(cbxDisciplina, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(cbxTurma, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(lblNomeCurso3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cbxTurma, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(lblNomeCurso2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cbxCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -274,7 +280,35 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
     }//GEN-LAST:event_tabelaAlunosMouseClicked
 
     private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
-        // TODO add your handling code here:
+        MatriculaController matriculasController = new MatriculaController();
+        String sql = "select m from Matriculas m where m.alunosId.nome like '%"+txtNomeAluno.getText()+"%' ";
+        Object obj = cbxCurso.getSelectedItem();
+        Cursos cursos = null;
+        if (obj instanceof Cursos) {
+            cursos = (Cursos) obj;
+        }
+        if(cursos!=null){
+            sql=sql+" and m.turmasId.cursosId.id="+cursos.getId();
+        }
+        obj = cbxTurma.getSelectedItem();
+        Turmas turmas = null;
+        if (obj instanceof Turmas) {
+            turmas = (Turmas) obj;
+        }
+        if(turmas!=null){
+            sql=sql+" and m.turmasId.id="+turmas.getId();
+        }
+        sql = sql + " order by m.alunosId.nome";
+        listaAlunos = matriculasController.listar(sql);
+        if (listaAlunos == null) {
+            listaAlunos = new ArrayList<Matriculas>();
+        }
+        modelAlunos = new ConsultaAlunosTableModel(listaAlunos);
+        tabelaAlunos.setModel(modelAlunos);
+        tabelaAlunos.getColumnModel().getColumn(0).setPreferredWidth(45);
+        tabelaAlunos.getColumnModel().getColumn(1).setPreferredWidth(200);
+        tabelaAlunos.getColumnModel().getColumn(2).setPreferredWidth(200);
+        tabelaAlunos.repaint();
     }//GEN-LAST:event_btnPesquisarActionPerformed
 
     private void btnNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoActionPerformed
@@ -286,7 +320,7 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
         int linha = tabelaAlunos.getSelectedRow();
         if (linha>=0){
              new FrmCadAluno(listaAlunos.get(linha), this);
-        }
+        }else JOptionPane.showMessageDialog(rootPane, "Selecione um Aluno");
     }//GEN-LAST:event_btnEditarActionPerformed
 
     private void cbxTurmaItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbxTurmaItemStateChanged
@@ -297,14 +331,6 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
         }
     }//GEN-LAST:event_cbxTurmaItemStateChanged
 
-    private void cbxDisciplinaItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbxDisciplinaItemStateChanged
-        Object obj = cbxDisciplina.getSelectedItem();
-        Disciplinas disciplinas = null;
-        if (obj instanceof Disciplinas) {
-            disciplinas = (Disciplinas) obj;
-        }
-    }//GEN-LAST:event_cbxDisciplinaItemStateChanged
-
     private void cbxCursoItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbxCursoItemStateChanged
         Object obj = cbxCurso.getSelectedItem();
         Cursos cursos = null;
@@ -312,8 +338,7 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
             cursos = (Cursos) obj;
         }
         if (cursos!=null){
-            carregarComboTurma(cursos);
-            carregarComboDisciplina(cursos);
+            carregarComboTurma(cursos); 
         }
     }//GEN-LAST:event_cbxCursoItemStateChanged
 
@@ -321,14 +346,35 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
       
     }//GEN-LAST:event_cbxCursoActionPerformed
 
+    private void btnLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimparActionPerformed
+        txtNomeAluno.setText("");
+        cbxCurso.setSelectedItem("Selecione");
+        cbxTurma.setSelectedItem(null);
+        carregarComboTurma(null);
+        setModel();
+    }//GEN-LAST:event_btnLimparActionPerformed
+
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
+        MatriculasFacade matriculasFacade = new MatriculasFacade();
+        int linha = tabelaAlunos.getSelectedRow();
+        if (linha>=0){
+            try {
+                matriculasFacade.excluir(listaAlunos.get(linha).getId());
+            } catch (SQLException ex) {
+                Logger.getLogger(FrmConsAluno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            setModel();
+        }else JOptionPane.showMessageDialog(rootPane, "Selecione um Aluno");
+    }//GEN-LAST:event_btnExcluirActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnExcluir;
+    private javax.swing.JButton btnLimpar;
     private javax.swing.JButton btnNovo;
     private javax.swing.JButton btnPesquisar;
     private javax.swing.JComboBox<String> cbxCurso;
-    private javax.swing.JComboBox<String> cbxDisciplina;
     private javax.swing.JComboBox<String> cbxTurma;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
@@ -337,7 +383,6 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
     private javax.swing.JLabel lblNomeCurso1;
     private javax.swing.JLabel lblNomeCurso2;
     private javax.swing.JLabel lblNomeCurso3;
-    private javax.swing.JLabel lblNomeCurso4;
     private javax.swing.JTable tabelaAlunos;
     private javax.swing.JTextField txtNomeAluno;
     // End of variables declaration//GEN-END:variables
@@ -367,21 +412,11 @@ public class FrmConsAluno extends javax.swing.JFrame implements IAlunos {
             if (listaTurmas != null) {
                 cbxTurma = Formatacao.preencherComobox(listaTurmas, cbxTurma, true, "Selecione");
             }
+        }else{
+            cbxTurma.removeAllItems();
         }
     }
-
-    public void carregarComboDisciplina(Cursos cursos) {  
-        if(cursos!=null){ 
-            cbxDisciplina.removeAllItems();
-            DisciplinaController disciplinaController = new DisciplinaController();
-            String sql = "select d from Disciplinas d where d.cursos.id="+cursos.getId()+" order by d.nome";
-            List<Disciplinas> listaDisciplina = disciplinaController.listar(sql);
-            if (listaDisciplina != null) {
-                cbxDisciplina = Formatacao.preencherComobox(listaDisciplina, cbxDisciplina, true, "Selecione");
-            }
-        }
-    }
-    
+ 
     public void carregarComboCurso() {
         CursosController cursosController = new CursosController();
         String sql = "select c from Cursos c order by c.nome";
