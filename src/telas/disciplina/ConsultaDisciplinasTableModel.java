7d1e664bf30613376f2317a9e2e4c257b2f1a2ae
phmarques <phmarques@PHMarques>
2016-10-25 21:12:58 -0200

    Consulta Disciplina

--- src/telas/disciplina/ConsultaDisciplinasTableModel.java
+++ src/telas/disciplina/ConsultaDisciplinasTableModel.java
@@ -1,15 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package telas.disciplina;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Disciplinas;
 
 /**
  *
- * @author Kamila
+ * @author Alecsander
  */
-public class ConsultaDisciplinasTableModel{
+public class ConsultaDisciplinasTableModel extends AbstractTableModel{
+    
+    private List<Disciplinas> listaDisciplina;
+    private String[] colunas = {"Nome","Curso"};
+    
+    public ConsultaDisciplinasTableModel(List<Disciplinas> listaDisciplina){
+        this.listaDisciplina = listaDisciplina;
+    }
+
+    @Override
+    public int getRowCount() {
+       return listaDisciplina.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        if (columnIndex==0){
+            return listaDisciplina.get(rowIndex).getNome();
+        }else return listaDisciplina.get(rowIndex).getCursos().getNome();
+        
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+        return colunas[column];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        if (columnIndex==0){
+            return String.class;
+        }else return String.class;
+    } 
+
+    
+    
     
     
     
