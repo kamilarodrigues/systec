12cd324723a410d435d5f00426830eb9afb60eef
Kamila <Kamila@Kamila-PC>
2016-11-04 11:52:34 -0200

    alinhando codigo disciplina

--- src/telas/disciplina/FrmCadDisciplina.java
+++ src/telas/disciplina/FrmCadDisciplina.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package telas.disciplina;
 
 import controller.CursosController;
@@ -10,12 +5,12 @@ import controller.DisciplinaController;
 import java.util.List;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import model.Cursos;
 import model.Disciplinas;
-import model.Matriculas;
-import telas.curso.*;
 import util.Formatacao;
 
 /**
@@ -23,17 +18,22 @@ import util.Formatacao;
  * @author Alecsander
  */
 public class FrmCadDisciplina extends JFrame {
-    
+
     private Disciplinas disciplina;
     private IDisciplina telaDisciplina;
     private Cursos curso;
-    
+    private javax.swing.JButton btnConfirmar, btnCancelar;
+    private JComboBox<String> cbxCurso;
+    private JPanel jPanel1;
+    private JLabel lblDisciplina, lblCurso;
+    private JTextField txtNomeDisciplina;
+
     public FrmCadDisciplina(Disciplinas disciplina, IDisciplina telaDisciplina) {
         this.disciplina = disciplina;
         this.telaDisciplina = telaDisciplina;
-        initComponents();
+        inserindoComponentes();
         carregandoComboCurso();
-        if (disciplina.getId() == null){
+        if (disciplina.getId() == null) {
             disciplina = new Disciplinas();
         } else {
             curso = disciplina.getCursos();
@@ -43,7 +43,7 @@ public class FrmCadDisciplina extends JFrame {
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
-    
+
     public void carregandoComboCurso() {
         CursosController cursosController = new CursosController();
         String sql = "select c from Cursos c order by c.nome";
@@ -52,18 +52,15 @@ public class FrmCadDisciplina extends JFrame {
             cbxCurso = Formatacao.preencherComobox(listaCursos, cbxCurso, true, "Selecione");
         }
     }
-    
-    
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
 
+    @SuppressWarnings("unchecked")
+    private void inserindoComponentes() {
         jPanel1 = new javax.swing.JPanel();
-        lblNomeCurso = new javax.swing.JLabel();
+        lblDisciplina = new javax.swing.JLabel();
         txtNomeDisciplina = new javax.swing.JTextField();
         btnConfirmar = new javax.swing.JButton();
-        btnPesquisar4 = new javax.swing.JButton();
-        lblNomeCurso1 = new javax.swing.JLabel();
+        btnCancelar = new javax.swing.JButton();
+        lblCurso = new javax.swing.JLabel();
         cbxCurso = new javax.swing.JComboBox<>();
 
         setTitle("Cadastro de Disciplina");
@@ -71,16 +68,11 @@ public class FrmCadDisciplina extends JFrame {
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        lblNomeCurso.setText("Nome da Disciplina:");
-        lblNomeCurso.setAlignmentY(0.1F);
+        lblDisciplina.setText("Nome da Disciplina:");
+        lblDisciplina.setAlignmentY(0.1F);
 
         txtNomeDisciplina.setAlignmentX(0.1F);
         txtNomeDisciplina.setAlignmentY(0.1F);
-        txtNomeDisciplina.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtNomeDisciplinaActionPerformed(evt);
-            }
-        });
 
         btnConfirmar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/confirmar.png"))); // NOI18N
         btnConfirmar.setText("Confirmar");
@@ -95,106 +87,89 @@ public class FrmCadDisciplina extends JFrame {
             }
         });
 
-        btnPesquisar4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/cancelar.png"))); // NOI18N
-        btnPesquisar4.setText("Cancelar");
-        btnPesquisar4.setBorder(null);
-        btnPesquisar4.setBorderPainted(false);
-        btnPesquisar4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        btnPesquisar4.setPreferredSize(new java.awt.Dimension(80, 90));
-        btnPesquisar4.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        btnPesquisar4.addActionListener(new java.awt.event.ActionListener() {
+        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/cancelar.png"))); // NOI18N
+        btnCancelar.setText("Cancelar");
+        btnCancelar.setBorder(null);
+        btnCancelar.setBorderPainted(false);
+        btnCancelar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnCancelar.setPreferredSize(new java.awt.Dimension(80, 90));
+        btnCancelar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnPesquisar4ActionPerformed(evt);
+                btnCancelarActionPerformed(evt);
             }
         });
 
-        lblNomeCurso1.setText("Curso:");
-        lblNomeCurso1.setAlignmentY(0.1F);
+        lblCurso.setText("Curso:");
+        lblCurso.setAlignmentY(0.1F);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtNomeDisciplina, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
-                            .addComponent(lblNomeCurso)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(76, 76, 76)
-                        .addComponent(btnConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnPesquisar4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(lblNomeCurso1))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(cbxCurso, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap(15, Short.MAX_VALUE))
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(txtNomeDisciplina, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+                                                        .addComponent(lblDisciplina)))
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addGap(76, 76, 76)
+                                                .addComponent(btnConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(lblCurso))
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(cbxCurso, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                .addContainerGap(15, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblNomeCurso)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtNomeDisciplina, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(lblNomeCurso1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(cbxCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnPesquisar4, javax.swing.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
-                    .addComponent(btnConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(lblDisciplina)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtNomeDisciplina, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(lblCurso)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cbxCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(btnCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
+                                        .addComponent(btnConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void btnPesquisar4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisar4ActionPerformed
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {
         this.dispose();
-    }//GEN-LAST:event_btnPesquisar4ActionPerformed
+    }
 
-    private void txtNomeDisciplinaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNomeDisciplinaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtNomeDisciplinaActionPerformed
-
-    private void btnConfirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConfirmarActionPerformed
-        // TODO add your handling code here:
+    private void btnConfirmarActionPerformed(java.awt.event.ActionEvent evt) {
         String validar = validarDados();
         if (validar.length() < 5) {
             preencherModelDisciplina();
         } else {
             JOptionPane.showMessageDialog(rootPane, validar);
         }
-    }//GEN-LAST:event_btnConfirmarActionPerformed
-
-    
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnConfirmar;
-    private javax.swing.JButton btnPesquisar4;
-    private javax.swing.JComboBox<String> cbxCurso;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel lblNomeCurso;
-    private javax.swing.JLabel lblNomeCurso1;
-    private javax.swing.JTextField txtNomeDisciplina;
-    // End of variables declaration//GEN-END:variables
+    }
 
     private String validarDados() {
         String dados = "";
